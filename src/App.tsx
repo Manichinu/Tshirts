25a50c44ccd76051cd0a6d03f631b9e0fc654ba2
Manikandan <manikumar31520@gmail.com>
2023-11-15 11:42:09 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="" element={<Navbar />} />
+        <Route path="/" element={<Navbar />} />
         <Route path="/cartitems" element={<CartItems />} />
       </Routes>
     </Router>
